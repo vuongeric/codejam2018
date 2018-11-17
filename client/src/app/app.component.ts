fb5e5f3be642e3d0a927dd066297a416e9b12205
Keverdose <yekevin1995@hotmail.com>
2018-11-17 12:10:53 -0500

    add angular CLI and new project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,46 +1,10 @@
-/**
- * Angular 2 decorators and services
- */
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { environment } from 'environments/environment';
-import { AppState } from './app.service';
+import { Component } from '@angular/core';
 
-export const ROOT_SELECTOR = 'app';
-
-/**
- * App Component
- * Top Level Component
- */
 @Component({
-  selector: ROOT_SELECTOR,
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: [
-    './app.component.css'
-  ],
+  selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public name = 'Angular Starter';
-  public tipe = 'assets/img/tipe.png';
-  public twitter = 'https://twitter.com/gdi2290';
-  public url = 'https://tipe.io';
-  public showDevModule: boolean = environment.showDevModule;
-
-  constructor(
-    public appState: AppState
-  ) {}
-
-  public ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-    console.log(this.appState);
-  }
-
+export class AppComponent {
+  title = 'app';
 }
-
-/**
- * Please review the https://github.com/AngularClass/angular-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
