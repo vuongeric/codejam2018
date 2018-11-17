983445f69e8f060043bbb10063eb76f155f03c3d
Dan Seremet <dan_seremet@yahoo.com>
2018-11-17 12:34:31 -0500

    Upload file

--- clientDan/CaptionThis/src/app/app.component.ts
+++ clientDan/CaptionThis/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CaptionThis';
+  selectedFile: File = null;
+  
+  constructor(private http: HttpClient) {}
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload(){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    this.http.post('url', fd, {
+      reportProgress: true,
+      observe: 'events'
+    })
+    .subscribe(event => {
+      if(event.type === HttpEventType.UploadProgress) {
+        console.log('Upload Progress: ' + Math.round(event.loaded/event.total) * 100 + '%');
+      } else if(event.type === HttpEventType.Response){
+        console.log(event);
+      }
+    })
+  }
 }
