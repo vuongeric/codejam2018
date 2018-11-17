a75eecf731f6bc88ae69eeb30595667ae6ac3182
Keverdose <yekevin1995@hotmail.com>
2018-11-17 14:31:45 -0500

    uploadfile works

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  selectedFile: File = null;
+  constructor(private http: HttpClient) { }
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+  onUpload() {
+    const fd = new FormData();
+    // const test = { "id": "1" };
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    // console.log(test);
+    console.log(fd);
+    this.http.post("http://localhost:5000/api/image/", fd).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
