8e54a8dc5b7fdce8fd9a25f4af7678052c71a480
Keverdose <yekevin1995@hotmail.com>
2018-11-17 21:36:51 -0500

    particle js

--- clientDan/CaptionThis/src/app/app.component.ts
+++ clientDan/CaptionThis/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpEventType, HttpHeaders} from '@angular/common/http';
-import {UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry} from 'ngx-file-drop';
-import {ICaptionThisResponse} from "./model/CaptionThisResponse";
-import {trigger, animate, style, group, animateChild, query, stagger, transition, state} from '@angular/animations';
-import {IQuote} from "./model/quote";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType, HttpHeaders } from '@angular/common/http';
+import { UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { ICaptionThisResponse } from "./model/CaptionThisResponse";
+import { trigger, animate, style, group, animateChild, query, stagger, transition, state } from '@angular/animations';
+import { IQuote } from "./model/quote";
 
 @Component({
   selector: 'app-root',
@@ -14,17 +14,17 @@ import {IQuote} from "./model/quote";
     trigger('simpleFadeAnimation', [
 
       // the "in" style determines the "resting" state of the element when it is visible.
-      state('in', style({opacity: 1})),
+      state('in', style({ opacity: 1 })),
 
       // fade in when created. this could also be written as transition('void => *')
       transition(':enter', [
-        style({opacity: 0}),
-        animate(600 )
+        style({ opacity: 0 }),
+        animate(600)
       ]),
 
       // fade out when destroyed. this could also be written as transition('void => *')
       transition(':leave',
-        animate(600, style({opacity: 0})))
+        animate(600, style({ opacity: 0 })))
     ])
   ]
 })
@@ -38,6 +38,135 @@ export class AppComponent {
   private currentQuote: IQuote;
   private currentIndex: number;
   private currentHashTags;
+
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
+  ngOnInit() {
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
+
+    this.myParams = {
+      "particles": {
+        "number": {
+          "value": 160,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#4a80be"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 1,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 1,
+            "opacity_min": 0,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 4,
+            "size_min": 0.3,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 1,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 600
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "window",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "bubble"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 250,
+            "size": 0,
+            "duration": 2,
+            "opacity": 0,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 400,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    };
+  }
 
   constructor(private http: HttpClient) {
   }
@@ -94,12 +223,12 @@ export class AppComponent {
   }
 
   nextQuote() {
-    if(this.quotes === undefined || this.quotes.length === 0) {
+    if (this.quotes === undefined || this.quotes.length === 0) {
       return;
     }
     this.showQuote = false;
-    setTimeout(()=> {
-      if(this.currentIndex === null) {
+    setTimeout(() => {
+      if (this.currentIndex === null) {
         this.currentIndex = 0;
         this.currentQuote = this.quotes[this.currentIndex];
         console.log(this.currentQuote);
--- clientDan/CaptionThis/src/app/app.module.ts
+++ clientDan/CaptionThis/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { FileDropModule } from 'ngx-file-drop';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatProgressSpinnerModule } from '@angular/material';
+import { ParticlesModule } from 'angular-particle';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { MatProgressSpinnerModule } from '@angular/material';
     BrowserAnimationsModule,
     MaterialModule,
     MatProgressSpinnerModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
